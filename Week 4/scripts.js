fa51efe6e01b7e37dbf6f81e608f24aa698d8800
ru <ruthdawit@ruths-macbook-air.home>
2020-05-10 19:38:56 +0200

    all the challenges

---
+++ Week 4/scripts.js
@@ -0,0 +1,110 @@
+/*FUNCTION WHO RETURNS THE AREA OF A SURFACE IN m2 
+--------------------------------------------------*/
+function totalArea(len, wid) {
+  let area = 'The area is ' + (len * wid) + ' m2';
+    show(area);
+ }
+ totalArea(2, 4);
+ 
+ 
+ /*FUNCTION WHO RETURNS THE REMAINDER OF TWO NUMBERS 
+ --------------------------------------------------*/
+   function remainder(div, divd) {
+    let rema = "The remainder is " + div%divd;
+    show(rema );
+ }
+ remainder(5, 4);
+ 
+ 
+ /*FUNCTION WHO RETURNS THE NUMBER OF FRAMES 
+ --------------------------------------------------*/
+  function frames(min, ps){
+  let fps = ps * 60;
+  show(min*fps);
+  }
+  frames(10, 25)
+ 
+ 
+ /*FUNCTION WHO RETURNS CITY FACTS 
+ --------------------------------------------------*/
+  let city = {
+   Ethiopia: {
+     name: "Ethiopia",
+     population: "2,140,526",
+     continent: "Africa"
+   },
+   France: {
+     name: "France",
+     population: "2,140,526",
+     continent: "Europe"
+   },
+   Paris: {
+     name: "Paris",
+     population: "2,140,526",
+     continent: "Europe"
+   }
+ }
+ function cityFact(city){
+   
+   show(city);
+ }
+ cityFact(city.Ethiopia)
+ 
+ 
+ /*FUNCTION THAT CALCULATE THE PROFIT
+ --------------------------------------------------*/
+ function profit(revenue, expenses){
+   let pro = revenue - expenses;
+   show(pro);
+ }
+ profit(60, 25);
+ 
+ 
+ /*FUNCTION THAT CHECK IF NUMBER IS LESS THAN OR EQUAL TO ZERO 
+--------------------------------------------------*/
+function number() {
+  let input= "";
+  let num= "";
+  if (num <= 0){
+    input= "The number is less than or equal to zero!"
+  }
+  else{
+    input= "The number is greater than zero"
+  }
+  show(input);
+ }
+ number(2);
+
+
+/*FUNCTION THAT CHECKS IF A NUMBER IS EVEN OR ODD 
+--------------------------------------------------*/
+ function even_odd() {
+  let num2= "";
+  if (even = 2*num2){
+    num2= "The number is even"
+  }
+  else{
+    num2= "The number is odd"
+  }
+  show(num2);
+ }
+ even_odd(9);
+
+
+ /*FUNCTION THAT CHECKS IF A WORD IS SINGULAR OR PLURAL 
+--------------------------------------------------*/
+function isPlural(word) {
+	if(word.endsWith('s')){
+		stat = "plural";
+	}
+	else {
+		stat= "singular";
+  }
+  show(stat);
+
+}
+isPlural("master");
+isPlural("sources");
+isPlural("element");
+
+ 
---
+++ Week 5/scripts.js
@@ -0,0 +1,112 @@
+/*TERNARY OPERATOR THAT CHECKS IF TODAY IS TUESDAY 
+--------------------------------------------------*/
+function tus(){
+let today = new Date().getDay();
+output = (today === 2) ? "Tuesday" : "No, today is not tuesday";
+show(output);
+}
+tus();
+
+
+/*FUNCTION THAT CHECKS EQUALITY OF 3 VALUES 
+--------------------------------------------------*/
+function equality(a, b, c){
+  if (a === b === c ){
+    txt = "They all are equal"
+  }
+  if (a === b || b === c || a === c){
+    txt = "Two of them are equal"
+  }
+  else {
+    txt = "They all are not equal"
+  }
+  show(txt);
+}
+equality(5, 5, 4);
+
+/*FUNCTION THAT CHECKS IF AN ARRAY IS 
+STRICTLY INCREASING OR DECREASING
+--------------------------------------------------*/
+
+function arr(array) {
+	for( i=1; i < array.length; i++){
+		 if(array[i] < array[i-1] && array[i-1] < array[i-2]){
+			msg= "decreasing";
+		 }else if(array[i] > array[i-1] && array[i-1] > array[i-2]){
+			msg=  "increasing";
+		} 
+		 else {
+			msg= "neither";
+    }
+  }
+  show(msg)
+
+}
+arr([1, 8, 0]);
+
+
+/*CREATE AN ARRAY CONTAINING 6 PERSONS GIRLS AND BOYS.
+--------------------------------------------------*/
+ let persons = [
+   {
+   name : "kidi",
+   gender : "female"
+  },
+   {
+   name : "juli",
+   gender : "female"
+  },
+   {
+   name : "abuta",
+   gender : "male"
+  },
+   {
+    name : "jani",
+    gender : "male"
+  },
+   {
+   name : "elshu",
+   gender : "male"
+  },
+   {
+   name : "kirubel",
+   gender : "male"
+  }
+ ];
+
+/*FUNCTION THAT CHECKS THE NUMBER OF BOYS AND GIRLS 
+--------------------------------------------------*/
+
+let female = 0;
+let male = 0;
+persons.forEach(function(element){
+  if( element.gender === "male"){
+    male++;
+
+    }
+  else {
+    female++;
+  }
+});
+
+show("There are " + female + " female and " + male + " male" );
+
+
+
+/* HTML DOM
+---------------------------------------- */
+// GET THE TEXT CONTENT
+const h1 = document.querySelector('h1');
+console.log(h1.innerHTML);
+
+// CHANGE THE TEXT
+h1.innerHTML = "patient is every thing!";
+console.log(h1.innerHTML);
+
+// ADD CLASS AND ID 
+h1.setAttribute('id', 'main-title');
+h1.setAttribute('class', 'title');
+
+
+
+
